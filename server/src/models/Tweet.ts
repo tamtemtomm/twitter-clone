3c696e821d7814b68d8f987820c599ff9fec6c84
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2024-07-31 20:44:53 +0700

    Update Tweet Schema

--- server/src/models/Tweet.ts
+++ server/src/models/Tweet.ts
@@ -13,6 +13,21 @@ const TweetSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  like:[{
+    userId: String
+  }],
+  retweet:[{
+    userId:String,
+    tweetId: String
+  }],
+  comment:[{
+    userId: String,
+    tweetId: String
+  }],
+  view:[{
+    userId: String
+  }]
+
 });
 
 const TweetModel = mongoose.model("tweets", TweetSchema);
